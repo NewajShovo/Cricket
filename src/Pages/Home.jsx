077ccdf18406c5d2a778730bcd41c785f00553ba
NewajShovo <leoshovo@gmail.com>
2023-01-29 11:43:41 +0600

    Home page with button

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import Button from '../Component/Button';
+const Home =() => {
+    return (
+        <div className="button-container no-rotation">
+            <Button label="Play with Friends" />
+            <Button label="Play with Stranger" />
+        </div>
+    );
+  }
+
+  export default Home;
