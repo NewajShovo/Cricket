0b90848776540875a8f4b19f71bf0f215681a468
NewajShovo <leoshovo@gmail.com>
2023-02-19 01:07:14 +0600

    Function triggered when player make a move.

--- src/Component/RunButton.js
+++ src/Component/RunButton.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 class RunButton extends Component {
+  handleClick = () => {
+    console.log(`Button clicked: ${this.props.label}, id: ${this.props.id}`);
+    // You can access the props here and use them as needed
+  };
   render() {
     return (
       <>
-        <button>{this.props.label}</button>
+        <button onClick={this.handleClick}>{this.props.label}</button>
       </>
     );
   }
