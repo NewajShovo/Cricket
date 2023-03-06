cfcb9ab1a5441aaf2309a6e336a6b820ac892d5c
NewajShovo <leoshovo@gmail.com>
2023-03-06 23:11:02 +0600

    Toss page.

--- src/Pages/Toss.jsx
+++ src/Pages/Toss.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { socket } from "../Component/Socket";
+import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 const Toss = () => {
-
+  const [currentPlayer1, setcurrentPlayer1] = useState(false);
+  const location = useLocation();
+  const navigate= useNavigate();
+  const { state } = location;
+  const currentPlayer = state.players[socket.id].identity;
+  console.log("Herrrreee.........",currentPlayer);
   const [isLoading, setIsLoading] = useState(true);
+  const makeToss =() => {
+    const tossResult = Math.floor(Math.random() * 10) + 1;
+    console.log(tossResult);
+    if(tossResult%2===1) setcurrentPlayer1(true);
+    else setcurrentPlayer1(false);
+    console.log(currentPlayer1,currentPlayer);
+  }
+
+  const redirectToPlayground=()=>{
+    const props = { 
+      player1_1stInnings: currentPlayer1
+    };
+    const urlSearchParams = new URLSearchParams(props);
+    window.location.href = `http://localhost:3001/Playground?${urlSearchParams.toString()}`;
+  }
 
   useEffect(() => {
+    makeToss();
     setTimeout(() => {
       setIsLoading(false);
     }, 2000);
+    setTimeout(() => {
+      redirectToPlayground();
+    }, 3000);
+
   }, []);
 
   return (
@@ -19,12 +48,23 @@ const Toss = () => {
             <div className="progress"></div>
           </div>
         ) : (
-          <h1 className="word">You will bat first</h1>
+
+          <h1 className="word">
+          { currentPlayer1
+            ? currentPlayer === "player1" 
+              ? "You will bat first"
+              : "You will ball first"
+            : 
+              currentPlayer === "player1" 
+              ? "You will ball first" 
+              : "You will bat first"
+          }
+          <Link to="/Playground">Go to Playground</Link>
+        </h1>
         )}
           </div>
       </div>
     </div>
-  
   )
 }
 
