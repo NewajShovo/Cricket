888403e52718429e13a47264e3d38136437e5c4d
NewajShovo <leoshovo@gmail.com>
2023-03-25 23:41:01 +0600

    socket.js improved

--- src/Component/Socket.js
+++ src/Component/Socket.js
@@ -1,7 +1,15 @@
 import io from "socket.io-client";
-export const socket = io(process.env.WEB_SERVICE, {
+export const socket = io("https://cricketserver.onrender.com", {
   transports: ["websocket"],
 });
 socket.on("connect", () => {
   console.log("Socket connected");
+});
+
+socket.on("connect_error", (error) => {
+  console.error("Failed to connect to server:", error);
+});
+
+socket.on("disconnect", () => {
+  console.warn("Disconnected from server.");
 });
