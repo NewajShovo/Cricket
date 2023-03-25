3df7871edd2d1e81a86a472d86a1bc11291aa354
NewajShovo <leoshovo@gmail.com>
2023-03-25 23:18:37 +0600

    Socket connection to web service put into environment variable.

--- src/Component/Socket.js
+++ src/Component/Socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-export const socket = io("http://localhost:3000", {
+export const socket = io(process.env.WEB_SERVICE, {
   transports: ["websocket"],
 });
 socket.on("connect", () => {
